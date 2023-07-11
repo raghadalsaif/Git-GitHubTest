f70fbdb738d9c3ef389254c56f7fc13994e205e3
RAGHAD ALSAIF <107502187+raghadalsaif@users.noreply.github.com>
2023-07-11 17:43:19 +0300

    Create SecondView

---
+++ Git&GitHubTest/SecondView.swift
@@ -0,0 +1,21 @@
+//
+//  SecondView.swift
+//  Git&GitHubTest
+//
+//  Created by raghad khalid alsaif on 23/12/1444 AH.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
