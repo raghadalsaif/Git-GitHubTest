e92883dfc1dcd8beba12b539b6d0f30c99eae564
RAGHAD ALSAIF <107502187+raghadalsaif@users.noreply.github.com>
2023-07-11 17:36:14 +0300

    Initial Commit

---
+++ Git&GitHubTest/Git_GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_GitHubTestApp.swift
+//  Git&GitHubTest
+//
+//  Created by raghad khalid alsaif on 23/12/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Git_GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
